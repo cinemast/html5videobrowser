b5a82b75e235e365080af1571e14ca41cbd7e7ac
Peter Spiess-Knafl <peter.spiess-knafl>
2014-01-27 00:07:24 +0100

    timeline with time

--- js/videobrowser.js
+++ js/videobrowser.js
@@ -3,6 +3,19 @@ timeline = null;
 ctx_timeline = null;
 container_timeline = null;
 
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    var time    = hours+':'+minutes+':'+seconds;
+    return time;
+}
+
 $(document).ready(function () {
 
     //jquery init function
@@ -14,23 +27,24 @@ $(document).ready(function () {
     timeline = $("#timeline");
     ctx_timeline = timeline[0].getContext("2d");
     ctx_timeline.canvas.width = container_timline.width();
-    ctx_timeline.canvas.height = 100;
+    ctx_timeline.canvas.height = 200;
 
-    drawTimeline(5);
+    drawTimeline(3600*2, 20);
 });
 
-function drawTimeline(interval) {
+//length in seconds, interval in divions units
+function drawTimeline(length, interval) {
 
     var increment = (timeline.width() / interval);
 
     //alert(increment);
 
-    for (i=0;i<timeline.width(); i+=increment){
+    for (i=0;i<=interval; i++){
        //ctx_timeline.lineWidth = 1;
        //alert(i + "/" + timeline.height());
        ctx_timeline.beginPath();
-       ctx_timeline.moveTo(i,50);
-       ctx_timeline.lineTo(i,timeline.height());
+       ctx_timeline.moveTo(i*increment,timeline.height()-50);
+       ctx_timeline.lineTo(i*increment,timeline.height()-10);
 
        ctx_timeline.lineWidth = 1;
 
@@ -38,6 +52,11 @@ function drawTimeline(interval) {
        ctx_timeline.strokeStyle = '#00086E';
 
        ctx_timeline.stroke();
+
+       ctx_timeline.font="10px Arial";
+       ctx_timeline.textAlign = 'center';
+       var timestring = "" + length/interval*i;
+       ctx_timeline.fillText(timestring.toHHMMSS(),i*increment,timeline.height());
     }
 
 }
